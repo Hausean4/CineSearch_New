82b39e9e8c86e5863be6c7f74a5fe5d1b882c7ad
Davud Demic <demicdav@students.zhaw.ch>
2023-02-08 16:27:20 +0100

    Moviesarch update

--- frontend/src/views/MovieSearch.vue
+++ frontend/src/views/MovieSearch.vue
@@ -5,12 +5,27 @@
         <ion-title>MovieSearch</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
+    <ion-content :fullscreen="true" padding>
+      <ion-grid>
+        <ion-row justify-content-center>
+          <ion-col align-self-center size-md="6" size-lg="5" size-xs="12">
+            <div padding>
+              <ion-item>
+                <ion-input type="text" placeholder="Movie Name" v-model="search"></ion-input>
+              </ion-item>
+            </div>
+            <div padding>
+              <ion-button size="large" @click="search" expand="block">Search</ion-button>
+            </div>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { search } from 'ionicons/icons';
 
 </script>
